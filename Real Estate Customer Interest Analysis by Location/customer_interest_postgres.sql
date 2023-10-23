f41e6856760c0b8585ffdfa49cd37574b906a155
Thanita.evj <145984336+thanita-evj@users.noreply.github.com>
2023-10-23 21:24:29 +0700

    Add files via upload

---
+++ Real Estate Customer Interest Analysis by Location/customer_interest_postgres.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customer_interest (
+	row_number VARCHAR(50),
+	transaction_year VARCHAR(50),
+	transaction_month VARCHAR(50),
+	province_name_en VARCHAR(50),
+	district_name_en VARCHAR(50),
+	propertytype_name_en VARCHAR(50),
+	volume_of_project INT,
+	interest INT
+);
